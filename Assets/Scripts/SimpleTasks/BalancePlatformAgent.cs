1ae7e2ee6d50bdb4efa64cff45988a3cbb034f7a
Félix Martins <felix.marcial@hotmail.com>
2025-06-24 20:04:56 +0100

    Renamed prefab

--- Assets/Scripts/SimpleTasks/BalancePlatformAgent.cs
+++ Assets/Scripts/SimpleTasks/BalancePlatformAgent.cs
@@ -24,6 +24,7 @@ public class BalancePlatformAgent : PlatformAgent
             SetReward(-1f);
             RecordFailure();
             EndEpisode();
+            return;
         }
         else
         {
